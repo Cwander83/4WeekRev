3fe1637500a30b9548a91d30ffa0d2332111be95
Chris Wander <31929558+Cwander83@users.noreply.github.com>
2019-06-17 15:11:07 -0400

    preview page

--- client/src/components/pages/WorkoutPreview.js
+++ client/src/components/pages/WorkoutPreview.js
@@ -1,13 +1,13 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 // import { connect } from "react-redux";
 //import { callingFullProgram } from "../../redux/Actions";
-import API from "../../api/API";
+import API from '../../api/API';
 
-import NavBox from "../NavBox";
+import NavBox from '../NavBox';
 
 class WorkoutPreview extends Component {
   state = {
-    workouts: []
+    workouts: [],
   };
   componentDidMount() {
     this.pullWorkoutPreview();
@@ -21,31 +21,65 @@ class WorkoutPreview extends Component {
   };
 
   render() {
-    console.log(`state: ${JSON.stringify(this.state, null, 2)}`);
+    //console.log(`state: ${JSON.stringify(this.state, null, 2)}`);
     //  console.log(`props: ${JSON.stringify(this.props, null, 2)}`);
     return (
       <div className="workoutPreview">
         <NavBox />
-        <h1>Welcome to WorkoutPreview Page</h1>
+
+        <header>
+          <h1>Sneak Peek of Four Week Revolution</h1>
+        </header>
         {// mapping thru first object
         this.state.workouts.map(all => (
           <div key={all._id} className="section">
-            {// mapping thru array of exercises
-            all.workouts.map(data => (
-              <div key={data._id} className="subSection">
-                <h2>{data.exercisename}</h2>
-                <p>Muscle focus: {data.bodypart}</p>
-                <p>Total sets: {data.totalsets}</p>
-                <p>
-                  # of Warmup sets: {data.warmupsets}
-                  {data.warmupreps} reps
-                </p>
-                <p />
-                <p>
-                  # of sets: {data.regularsets} x {data.regularreps} reps
-                </p>
+            <h1>Day 1 - Quads and Hamstrings</h1>
 
-                <p>How-to: {data.regulardesc}</p>
+            {// mapping thru array of exercises
+            all.workouts.map((data, index) => (
+              <div key={index} className="subSection">
+                <h2>{data.exercisename}</h2>
+                <div className="workout">
+                  {
+                    // <p>Muscle focus: {data.bodypart}</p>  <p>{data.totalsets} totalsets</p>
+                  }
+                  <div className="col-1">
+                    {// if no warm up then not showing
+                    !data.warmupsets ? (
+                      <div />
+                    ) : (
+                      <div>
+                        <h3>Warm-up</h3>
+                        <p>
+                          <span>Sets:</span> {data.warmupsets} x{' '}
+                          {data.warmupreps}
+                        </p>
+
+                        <p>
+                          <span>How-to:</span> {data.warmupdesc}
+                        </p>
+                      </div>
+                    )}
+                    <h3>Strategy</h3>
+                    <p>
+                      <span>Sets:</span> {data.regularsets} x {data.regularreps}
+                    </p>
+
+                    <p>
+                      <span>How-to:</span> {data.regulardesc}
+                    </p>
+                  </div>
+                  <div className="col-2">
+                    <img
+                      src="https://via.placeholder.com/200x200"
+                      alt="dummy"
+                    />
+                    <img
+                      src="https://via.placeholder.com/200x200"
+                      alt="dummy"
+                    />
+                  </div>
+                </div>
               </div>
             ))}
           </div>
